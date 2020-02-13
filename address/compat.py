8e3df0ac615f03627d6b4c0a74eb26716ba9cc4d
Omen Apps <support@omenapps.com>
2020-02-12 20:57:37 -0500

    Removed deprecated argument

--- address/compat.py
+++ address/compat.py
@@ -1,9 +1,5 @@
-import django
 from django.db.models.fields.related import ForeignObject
-
-django_version = django.VERSION
 
 
 def compat_contribute_to_class(self, cls, name, virtual_only=False):
-    super(ForeignObject, self).contribute_to_class(
-        cls, name, virtual_only=virtual_only)
+    super(ForeignObject, self).contribute_to_class(cls, name)
