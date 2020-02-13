caa0e835f27b7bf5cefea6b7dedc2fe722c0e3bb
Omen Apps <support@omenapps.com>
2020-02-12 21:01:35 -0500

    Updated to use correct argument

--- address/compat.py
+++ address/compat.py
@@ -1,5 +1,5 @@
 from django.db.models.fields.related import ForeignObject
 
 
-def compat_contribute_to_class(self, cls, name, virtual_only=False):
-    super(ForeignObject, self).contribute_to_class(cls, name)
+def compat_contribute_to_class(self, cls, name, private_only=False):
+    super(ForeignObject, self).contribute_to_class(cls, name, private_only=private_only)
