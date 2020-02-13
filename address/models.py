c7272a28f5ccb60efa41ea9c6d5dcf2d7e7d9d88
Omen Apps <support@omenapps.com>
2020-02-12 21:03:50 -0500

    Corrected argument in app

--- address/models.py
+++ address/models.py
@@ -310,11 +310,11 @@ class AddressField(models.ForeignKey):
         kwargs['to'] = 'address.Address'
         super(AddressField, self).__init__(*args, **kwargs)
 
-    def contribute_to_class(self, cls, name, virtual_only=False):
+    def contribute_to_class(self, cls, name, private_only=False):
         from address.compat import compat_contribute_to_class
 
-        compat_contribute_to_class(self, cls, name, virtual_only)
-        # super(ForeignObject, self).contribute_to_class(cls, name, virtual_only=virtual_only)
+        compat_contribute_to_class(self, cls, name, private_only)
+        # super(ForeignObject, self).contribute_to_class(cls, name, private_only=private_only)
 
         setattr(cls, self.name, AddressDescriptor(self))
 
