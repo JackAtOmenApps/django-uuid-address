b3f7f3141842cd73a709230c1d88f736d82fad36
Omen Apps <support@omenapps.com>
2022-08-29 18:49:47 -0400

    Update to use newer functions
    https://docs.djangoproject.com/en/3.0/releases/3.0/#id3

--- address/models.py
+++ address/models.py
@@ -5,7 +5,7 @@ import uuid
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.fields.related import ForeignObject
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 try:
